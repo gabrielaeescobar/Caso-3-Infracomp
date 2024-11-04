dc69f88d92e74d25d86d10f60f6059f2b50acb20
Gabriela Escobar <g.escobar23@uniandes.edu.co>
2024-11-03 21:17:52 -0500

    opciones de inicio

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,37 +1,58 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Servidor {
 
     public static void main(String args[]) throws IOException {
-    ServerSocket ss = null;
-    boolean continuar = true;
-    int threadId = 0; 
+    boolean continuar0 = true;
+    
+    while (continuar0){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido al Servidor. Seleccione una opción:");
+        System.out.println("1: Generar pareja de llaves asimétricas del servidor y almacenarlas en archivos");
+        System.out.println("2: Ejecutar creando los delegados (threads) para cada cliente");
+        System.out.println("3: Salir");
 
-    System.out.println("Main Server ...");
+        int opcion = scanner.nextInt();
+        if (opcion == 1) {
+            System.out.println("Las llaves han sido generadas y almacenadas en archivos.");
 
-    try {
-        ss = new ServerSocket(3400);
-    } catch (IOException e) {
-        System.err.println("No se pudo crear el socket en el puerto: " );
-        System.exit(-1);
+        }
+        else if (opcion == 2){
+            ServerSocket ss = null;
+            boolean continuar = true;
+            int threadId = 0; 
+        
+            System.out.println("Main Server ...");
+        
+            try {
+                ss = new ServerSocket(3400);
+            } catch (IOException e) {
+                System.err.println("No se pudo crear el socket en el puerto: " );
+                System.exit(-1);
+            }
+        
+            while (continuar) {
+                // crear el thread y lanzarlo.
+        
+                // crear el socket
+                Socket socket = ss.accept();
+        
+                // crear el thread con el socket y el id
+                ThreadServidor thread = new ThreadServidor(socket, threadId++);
+                // aquí debe hacer una modificación porque todos los threads tienen un identificador diferente
+        
+                // start
+                thread.start();
+            }
+            ss.close();
+        }
+        else if (opcion==3) continuar0 = false;
+
     }
 
-    while (continuar) {
-        // crear el thread y lanzarlo.
-
-        // crear el socket
-        Socket socket = ss.accept();
-
-        // crear el thread con el socket y el id
-        ThreadServidor thread = new ThreadServidor(socket, threadId++);
-        // aquí debe hacer una modificación porque todos los threads tienen un identificador diferente
-
-        // start
-        thread.start();
-    }
-    ss.close();
 }
 
 
