6021652df97593b37b13c83d1b0c9293c7d53798
MariaaPinzon <ma.pinzonr1@uniandes.edu.co>
2024-11-04 12:36:28 -0500

    metodo para enviar uid cifrado

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,24 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 
 public class Cliente {
     public static final int PUERTO = 3400;
     public static final String SERVIDOR = "localhost";
+    public int uid;
     
     public static void main(String[] args) throws IOException {
         Socket socket = null;
         PrintWriter escritor = null;
         BufferedReader lector = null;
         BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        Random random = new Random();
+        int uid = random.nextInt(10000); // Número aleatorio entre 0 y 9999
+        System.out.println("UID del cliente: " + uid);
 
-
-        System.out.println("Comienza cliente");
-        try {
+        System.out.println("Comienza cliente");        try {
             socket = new Socket(SERVIDOR, PUERTO);
             escritor = new PrintWriter(socket.getOutputStream(), true);
             lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            ProtocoloCliente.procesar(teclado, lector, escritor);
+            ProtocoloCliente.procesar(teclado, lector, escritor,uid);
 
         } catch (Exception e) {
             e.printStackTrace();
